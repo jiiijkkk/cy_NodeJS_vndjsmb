47c2bd88c9ea71332131b13d9f11b613dcc92223
jiiijkkk <jiiijkkk@yahoo.com.tw>
2012-10-17 11:46:17 +0800

    Backup before sessions adding.

--- routes/myModule/sessionsManager.js
+++ routes/myModule/sessionsManager.js
@@ -1,14 +1,8 @@
-var sessions=   require("sessions"),
-    sessionHandler = new sessions();
+
 
 exports.setSession = function(req, res){
-    sessionHandler.httpRequest(req, res, function (err, session){
-        if (err) {
-            return res.end("session error");
-        }
-        console.log("[s] > %s", session.get("a"), req.url);
-        console.log("aa");
-        res.end(req.url);
-    });
+    console.log(req);
+    console.log(req.sessionID);
+    req.session.test = req.param('test');
 //    console.log(session);
 }
